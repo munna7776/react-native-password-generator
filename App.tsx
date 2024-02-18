add0bad59e2d96a1fb715ef8e371548b09a20f7c
Munna Kumar Barnwal <barnwalmunnakumar@gmail.com>
2024-02-18 13:12:21 +0530

    Initialized password generator in react native

--- App.tsx
+++ App.tsx
@@ -1,95 +1,70 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import React, { useState } from 'react';
+import * as Yup from 'yup';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const LOWER_CASE = 'abcdefghijklmnopqrstuvwxyz';
+const DIGITS = '1234567890';
+const SYMBOLS = '@#$&.!'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const PasswordSchema = Yup.object().shape({
+  passwordLength: Yup.number()
+    .min(4, 'Should be of min 4 characters')
+    .max(16, 'Should be of max 16 characters')
+    .required('Password Length is required.'),
+});
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+export default function App() {
+  const [password, setPassword] = useState("")
+  const [hasLowerCase, setHasLowerCase] = useState(false)
+  const [hasUpperCase, setHasUpperCase] = useState(false)
+  const [hasDigit, setHasDigit] = useState(false)
+  const [hasSymbols, setHasSymbols] = useState(false)
+  const [isPasswordGenerated, setIsPasswordGenerated] = useState(false)
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = ""
+    for(let i=0;i<passwordLength;i++){
+      const randomCharacterIndex = Math.round(Math.random() * characters.length)
+      result += characters.charAt(randomCharacterIndex)
+    }
+    return result
+  }
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const generatePasswordString = (passwordLength: number) => {
+    let characters = ""
+    if(hasLowerCase) {
+      characters += LOWER_CASE
+    }
+    if(hasUpperCase) {
+      characters += UPPER_CASE
+    }
+    if(hasDigit) {
+      characters += DIGITS
+    }
+    if(hasSymbols) {
+      characters += SYMBOLS
+    }
+    const password = createPassword(characters, passwordLength)
+    setIsPasswordGenerated(true)
+    return password
+  }
+
+  const resetPasswordState = () => {
+    setPassword("")
+    setHasDigit(false)
+    setHasUpperCase(false)
+    setHasLowerCase(false)
+    setHasSymbols(false)
+    setIsPasswordGenerated(false)
+  }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView>
+      <ScrollView style={styles.parentContainer}>
+        <View>
+          <Text style={styles.headingText}>Password Generator</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -97,22 +72,14 @@ function App(): React.JSX.Element {
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  parentContainer: {
+    backgroundColor: '#414141',
+    height: '100%',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  headingText: {
+    fontSize: 27,
+    lineHeight: 31,
+    color: '#fff',
+    padding: 10,
   },
 });
-
-export default App;
